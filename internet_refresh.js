91bfd12f5b03a1f01ae3bcd3210d4be41248aea6
Masud Rana <91444955+masudprobd@users.noreply.github.com>
2025-03-25 09:57:18 +0600

    Update internet_refresh.js

--- internet_refresh.js
+++ internet_refresh.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    if (!navigator.onLine) {
-        setTimeout(function(){
-            location.reload(); // ইন্টারনেট না থাকলে প্রতি ১ সেকেন্ডে রিফ্রেশ হবে
-        }, 1000);
-    }
+    setInterval(function() {
+        if (!navigator.onLine) {
+            location.reload(); // ইন্টারনেট না থাকলে প্রতি ৩ সেকেন্ডে রিফ্রেশ হবে
+        }
+    }, 3000);
 };
